4178f7e8b87a32ccc6e5832e4c2e890a807979ae
Remco Haszing <remcohaszing@gmail.com>
2022-04-07 11:15:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Monaco marker data provider
+
+[![ci workflow](https://github.com/remcohaszing/monaco-marker-data-provider/actions/workflows/ci.yaml/badge.svg)](https://github.com/remcohaszing/monaco-marker-data-provider/actions/workflows/ci.yaml)
+[![npm version](https://img.shields.io/npm/v/monaco-marker-data-provider)](https://www.npmjs.com/package/monaco-marker-data-provider)
+[![prettier code style](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://prettier.io)
+
+Provider marker data for Monaco models.
+
+Monaco editor supports marker data to highlight parts of a document using squiggly lines. This is
+analogous to diagnostics in the language server protocol. Monaco editor itself only supports this
+through the `editor.setModelMarkers` function. This allows users to set marker data even if a file
+isn’t open. This is useful for situations where a change in one file may lead to a marker data
+change in another. However, in many cases models don’t depend on each other to determine marker
+data. This is where `monaco-marker-data-provider` comes in. It exposes an API to set model markers
+using a provider, in a similar fashion as other Monaco providers.
+
+## Installation
+
+This project has a peer dependency on `monaco-editor`. It’s recommended to add it explicitly.
+
+```sh
+npm install monaco-editor monaco-marker-data-provider
+```
+
+## Usage
+
+```typescript
+import * as monaco from 'monaco-editor';
+import { registerMarkerDataProvider } from 'monaco-marker-data-provider';
+
+import { myCustomValidator } from './myCustomValidator';
+
+registerMarkerDataProvider(monaco, 'plaintext', {
+  owner: 'my-custom-markers',
+
+  provideMarkerData(model) {
+    return myCustomValidator(model.getValue());
+  },
+});
+```
+
+## API
+
+### `registerMarkerDataProvider(monaco, languageId, provider)`
+
+This function returns a disposable.
+
+#### Options
+
+- `monaco`: The `monaco-editor` module instance.
+- `languageId` (`string`): The language id to register the provider for.
+- `provider` (`MarkerDataProvider`): The object which provides Monaco marker data.
+
+## Types
+
+### `MarkerDataProvider`
+
+An object with the following properties:
+
+- `owner`: A unique string to determine the owner of the marker data. This may be used to retrieve
+  the marker data using `editor.getModelMarkers(owner)`.
+- `provideMarkerData`: A function which provides marker data for the given model. It must return
+  `null`, `undefined`, a list of marker data, or a promise that returns those values.
+- `doReset`: An optional function to perform a reset after a model has been removed.
+
+## License
+
+[MIT](https://github.com/remcohaszing/monaco-marker-data-provider/blob/main/LICENSE.md)
---
+++ index.ts
@@ -0,0 +1,106 @@
+import { editor, IDisposable, languages } from 'monaco-editor';
+
+export interface MarkerDataProvider {
+  /**
+   * The owner of the model markers.
+   *
+   * This should be a unique string that identifies the context of who owns the marker data.
+   */
+  owner: string;
+
+  /**
+   * Provide marker data for the given model.
+   *
+   * @param model - The model to provide marker data for.
+   * @returns The new marker data for the model.
+   */
+  provideMarkerData: (model: editor.ITextModel) => languages.ProviderResult<editor.IMarkerData[]>;
+
+  /**
+   * Reset the state for a model.
+   *
+   * @param model - The model to reset the state for.
+   */
+  doReset?: (model: editor.ITextModel) => void;
+}
+
+/**
+ * Register a marker data provider that can provide marker data for a model.
+ *
+ * @param monaco - The Monaco editor module.
+ * @param languageId - The language id to register the provider for.
+ * @param provider - The provider that can provide marker data.
+ * @returns A disposable.
+ */
+export function registerMarkerDataProvider(
+  monaco: Pick<typeof import('monaco-editor'), 'editor'>,
+  languageId: string,
+  provider: MarkerDataProvider,
+): IDisposable {
+  const listeners = new Map<string, IDisposable>();
+
+  const doValidate = async (model: editor.ITextModel): Promise<void> => {
+    const markers = await provider.provideMarkerData(model);
+    // The model have have been disposed by the time marker data has been fetched.
+    if (!model.isDisposed() && model.getLanguageId() === languageId) {
+      monaco.editor.setModelMarkers(model, provider.owner, markers ?? []);
+    }
+  };
+
+  const onModelAdd = (model: editor.IModel): void => {
+    if (model.getLanguageId() !== languageId) {
+      return;
+    }
+
+    let handle: ReturnType<typeof setTimeout>;
+    listeners.set(
+      String(model.uri),
+      model.onDidChangeContent(() => {
+        clearTimeout(handle);
+        handle = setTimeout(() => {
+          doValidate(model);
+        }, 500);
+      }),
+    );
+
+    doValidate(model);
+  };
+
+  const onModelRemoved = (model: editor.IModel): void => {
+    monaco.editor.setModelMarkers(model, provider.owner, []);
+    const uriStr = String(model.uri);
+    const listener = listeners.get(uriStr);
+    if (listener) {
+      listener.dispose();
+      listeners.delete(uriStr);
+    }
+  };
+
+  const disposables = [
+    monaco.editor.onDidCreateModel(onModelAdd),
+    monaco.editor.onWillDisposeModel((model) => {
+      onModelRemoved(model);
+      provider.doReset?.(model);
+    }),
+    monaco.editor.onDidChangeModelLanguage((event) => {
+      onModelRemoved(event.model);
+      onModelAdd(event.model);
+      provider.doReset?.(event.model);
+    }),
+  ];
+
+  for (const model of monaco.editor.getModels()) {
+    onModelAdd(model);
+  }
+
+  return {
+    dispose() {
+      for (const listener of listeners.values()) {
+        listener.dispose();
+      }
+      while (disposables.length) {
+        disposables.pop()!.dispose();
+      }
+    },
+  };
+}
